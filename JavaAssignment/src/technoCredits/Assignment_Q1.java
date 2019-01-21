d3241e7b0822d2a93ce0fe3c91e8174716eed2e1
Krishna_Kanani <krishna.kanani@hp.com>
2019-01-21 14:41:44 +0530

    Assignment_Q1_Krishna

--- JavaAssignment/src/technoCredits/Assignment_Q1.java
+++ JavaAssignment/src/technoCredits/Assignment_Q1.java
@@ -2,7 +2,9 @@ package technoCredits;
 
 public class Assignment_Q1 {
 
+	// Added comment
 	public static void main(String[] args) {
 		System.out.println("Hello, welcome to Git");
+		System.out.println("Done");
 	}
 }
