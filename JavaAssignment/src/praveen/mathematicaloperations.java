3e08d0cb988e2a3c9cd977bd7673da21ae17afef
unknown <you@example.com>
2019-01-22 23:14:04 +0530

    adding file for assignments

---
+++ JavaAssignment/src/praveen/mathematicaloperations.java
@@ -0,0 +1,35 @@
+package praveen;
+
+public class mathematicaloperations {
+	static void Sum(int a, int b) {
+
+		System.out.println((a + b));
+	}
+
+	static void Sub(double a, double b) {
+
+		System.out.println((a - b));
+	}
+
+	static void Div(double a, double b) {
+
+		System.out.println((a / b));
+	}
+
+	static void Rem(double a, double b) {
+
+		System.out.println((a % b));
+	}
+	static void Multi(double a, double b) {
+
+		System.out.println((a * b));
+	}
+public static void main(String[] args) {
+	Sum(10, 20);
+	Div(5, 10);
+	Rem(13, 3);
+	Multi(5, 3);
+	Sub(10, 5);
+	
+}
+}
