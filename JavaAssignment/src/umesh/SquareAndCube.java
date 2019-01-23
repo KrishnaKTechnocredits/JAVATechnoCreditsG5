933ef1f804f051db230f185229fed23f6fbb2859
UmeshPTechnocredits <umesh.panse@gmail.com>
2019-01-23 22:48:19 +0530

    Assignment_Q3_Umesh

--- JavaAssignment/src/umesh/SquareAndCube.java
+++ JavaAssignment/src/umesh/SquareAndCube.java
@@ -1,12 +1,10 @@
 package umesh;
-class SquareAndCube
-{
-public static void main(String args[])
-{
-System.out.println("Number\tSquare\tCube");
-for(int i=1;i<20;i++)
-{
-System.out.println(i+" "+i*i+" "+i*i*i);
-}
-}
+
+class SquareAndCube {
+	public static void main(String args[]) {
+		System.out.println("Number\tSquare\tCube");
+		for (int i = 1; i < 20; i++) {
+			System.out.println(i + " " + i * i + " " + i * i * i);
+		}
+	}
 }
