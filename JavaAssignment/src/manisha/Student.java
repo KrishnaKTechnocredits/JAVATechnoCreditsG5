648b93937a46287d945330feaf1f12ce248d9a18
unknown <m.sharma887@gmail.com>
2019-01-24 16:27:50 +0530

    Adding File for 2nd assignment

---
+++ JavaAssignment/src/manisha/Student.java
@@ -0,0 +1,14 @@
+package manisha;
+
+public class Student {
+
+	public static void main(String[] args) {
+
+		int i = Integer.parseInt(args[2]);
+		System.out.println("marks of the student " + args[1] + " is " + i);
+		System.out.println("Roollnum is " + args[0]);
+		System.out.println("Name of the Student is " + args[1]);
+
+	}
+
+}
